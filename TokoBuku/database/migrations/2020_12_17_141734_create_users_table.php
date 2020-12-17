cd3be7535f197f739005ab90cb4fe95b02a89cb3
dntffm <dentamauuulana99@gmail.com>
2020-12-17 22:04:17 +0700

    migrations success

--- TokoBuku/database/migrations/2020_12_17_141734_create_users_table.php
+++ TokoBuku/database/migrations/2020_12_17_141734_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->bigIncrements('role_id');
+            $table->unsignedBigInteger('role_id');
             $table->rememberToken();
             $table->foreign('role_id')->references('id')->on('roles');
             $table->timestamps();
