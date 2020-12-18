695dfab2a7f98aafb7e676c6d86e171ac07cbedf
dntffm <dentamauuulana99@gmail.com>
2020-12-18 13:41:04 +0700

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(BookCategoriesTableSeeder::class);
+        $this->call(BooksTableSeeder::class);
+        $this->call(BookCategorySeeder::class);
+        
     }
 }
